e19f200a2ebfc5c49931b8f44e2a4b15486abaa7
Cristian-Gabriel <lottus98@gmail.com>
2020-07-10 23:23:15 +0300

    Site PAW

---
+++ CinemaApp/CinemaApp/CinemaApp/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models
+{
+    public class Reservation
+    {
+        public int ID { get; set; }
+
+        public DateTime ReservedDate {get;set;}
+
+        public decimal Duration{ get; set; } //egala cu durata filmului
+
+        public int? MovieID { get; set; }
+
+        public Movie Movie { get; set; }
+
+        public ICollection<UserReservation> UserReservation { get; set; }
+
+
+    }
+}
---
+++ CinemaApp/CinemaApp/CinemaApp/Models/UserReservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Models
+{
+    public class UserReservation
+    {
+        public string UserID { get; set; }
+
+        public MyUser MyUser { get; set; }
+
+        public int ReservationID { get; set; }
+
+        public Reservation Reservation { get; set; }
+    }
+
+}
